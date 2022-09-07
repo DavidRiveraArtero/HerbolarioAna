129007b89ca09b43cafbabafe77ba459618449db
DavidRiveraArtero <davidtoy222@gmail.com>
2022-09-07 12:49:25 +0200

    Carousel Productos

--- src/app/component/UserPage/producto-carrousel/producto-carrousel.component.ts
+++ src/app/component/UserPage/producto-carrousel/producto-carrousel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+
 @Component({
   selector: 'app-producto-carrousel',
   templateUrl: './producto-carrousel.component.html',
@@ -9,45 +10,86 @@ export class ProductoCarrouselComponent implements OnInit {
 
  // Carousel
 
- scrollPosition = 0
+ scrollPosition:any = 0
  cardWidht:number = 0
  lenghCard:number = 0
- carouselWidth:number = 0
+ carouselWidth:any
+
+
  constructor() { }
 
+ categorias = [
+  {
+    nombre:"Organico"
+  },
+  {
+    nombre:'Paraser' // POR ALGUN MOTIVO LA PRIMERA LETRA TIENE QUE SER EN MAYUSCULA PORQUE SI NO DA UN ERROR
+  }
+ ]
+
  ngOnInit(): void {
-
-   this.carouselWidth = $('.carousel-p')[0].scrollWidth;
-
-   this.cardWidht = $('.carousel-i').width()!
-   this.lenghCard = $('.carousel-i').length
-
-   console.log(this.lenghCard)
  }
 
 
 
- next(){
-    if(this.scrollPosition < (this.carouselWidth - (this.cardWidht * (this.lenghCard - 1)))){
-      console.log(this.lenghCard)
-      this.scrollPosition = this.scrollPosition + this.cardWidht
-      $('.carousel-p').animate({
-        scrollLeft:this.scrollPosition
-      },600)
-    }
-   console.log('click',this.cardWidht)
+
+
+ next(event:MouseEvent){
+
+
+  var cardClass:any = (((event.target as Element).parentNode?.parentNode?.firstChild)?.firstChild as Element).classList[0]
+  var clasAnim:any = ((event.target as Element).parentNode?.parentNode?.firstChild as Element).classList[0]
+
+  console.log(clasAnim)
+
+  this.cardWidht = $('.'+cardClass).width()!
+  this.lenghCard = $('.'+cardClass).length
+  this.carouselWidth = $('.'+clasAnim)[0].scrollWidth
+
+
+
+   console.log("cardWith: ", this.cardWidht)
+   console.log("lenght: ", this.lenghCard)
+   console.log("carouselWidth", this.carouselWidth)
+
+
+  if(this.scrollPosition < (this.carouselWidth - (this.cardWidht * (this.lenghCard - 1)))){
+
+
+    this.scrollPosition = this.scrollPosition + (this.carouselWidth / this.lenghCard)
+    console.log("scrollPosition: " + this.scrollPosition)
+
+    $('.'+clasAnim).animate({
+      scrollLeft:this.scrollPosition
+    },400)
+
+  }else{
+    this.scrollPosition = 0
+  }
+
+
+
 
  }
 
- prev(){
-   if(this.scrollPosition > 1){ // EL 4 cambiar por la longitud del numero de card
-    console.log("dentro de prev")
-     this.scrollPosition = this.scrollPosition - this.cardWidht
-     $('.carousel-p').animate({
-       scrollLeft:this.scrollPosition
-     },600)
-   }
-   console.log('click',this.cardWidht)
+ prev(event:MouseEvent){
+
+  var cardClass:string = (((event.target as Element).parentNode?.parentNode?.firstChild)?.firstChild as Element).classList[0]
+  this.cardWidht = $('.'+cardClass).width()!
+
+  var clasAnim:string = ((event.target as Element).parentNode?.parentNode?.firstChild as Element).classList[0]
+
+  if(this.scrollPosition >= 1){
+    //console.log("dentro de prev")
+
+    this.scrollPosition = this.scrollPosition - (this.carouselWidth / this.lenghCard)
+    $('.'+clasAnim).animate({
+      scrollLeft:this.scrollPosition
+    },400)
+  }else{
+    this.scrollPosition = 2
+  }
+  //console.log('click',this.cardWidht)
 
  }
 
